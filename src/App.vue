048773b302337e67716dcbc69121f365c976124e
Baranov M.V. <mishasamarabaranov2002@mail.ru>
2025-07-12 03:12:45 +0400

    Base setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header>
+    
+  </header>
+  <main>
+    <router-view/>
+  </main>
 </template>
 
+<script setup lang="ts">
+
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+body {
+  min-height: 100vh;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app {
+  min-height: 100vh;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 </style>
